fd140f31fcf3f39dccd4ca90635a87a227ba7180
Brian Schiller <bgschiller@gmail.com>
2013-06-06 17:46:29 -0700

    bit more plumbing.

---
+++ rayage/websocket_message_handlers/test_list_request.py
@@ -0,0 +1,20 @@
+from constants import *
+
+import os
+import json
+import os.path
+
+from ..WebSocketHandler import messageHandler
+
+@messageHandler("test_list_request")
+def handle_project_list_request(socket_connection, message):
+    """
+    Writes a JSON structure representing the available projects to work on to our socket.
+    Currently a flat list of folders in the STUDENTS_DIR
+    """
+    projects = [{'label': p, 'id': p} for p in os.listdir(TESTS_DIR) 
+                                      if os.path.isdir(TESTS_DIR + p)]
+                                      
+    result_message = {'type': 'test_list',
+                      'tests': projects}
+    socket_connection.write_message(json.dumps(result_message))
